4eef18f0340e896b43e4705b3ea3f76c36b7224a
Krystian Kalwik <ambitious156288@gmail.com>
2021-11-11 00:28:41 +0100

    search mobile, RWD

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ const menu = document.querySelector(".menu")!;
 const searchButtonBottom = document.querySelector(".search-laptop__btn")!;
 const searchInputBottom = document.querySelector(".search-laptop__input")!;
 
-const closeNavigationButton = document.querySelector(".menu-mobile__button")!;
+const closeNavigationButton = document.querySelector(".menu-mobile-close")!;
+
+const searchMobileButton = document.querySelector(".search-mobile__btn")!;
+const searchMobileInput = document.querySelector(".search-mobile__input")!;
+const logo = document.querySelector(".logo")!;
+const searchCloseBtnMobile = document.querySelector(".menu-mobile__button")!;
 
 avatar.addEventListener("click", () => {
   menu.classList.toggle("menu--active");
@@ -16,4 +21,20 @@ searchButtonBottom.addEventListener("click", () => {
 
 closeNavigationButton.addEventListener("click", () => {
   menu.classList.toggle("menu--active");
+});
+
+searchMobileButton.addEventListener("click", () => {
+  searchMobileInput.classList.toggle("search-mobile__input--active");
+  logo.classList.toggle("hide");
+  searchMobileButton.classList.toggle("search-mobile__btn--change");
+  avatar.classList.toggle("hide");
+  searchCloseBtnMobile.classList.toggle("search-close-button--active");
+});
+
+searchCloseBtnMobile.addEventListener("click", () => {
+  logo.classList.toggle("hide");
+  searchMobileInput.classList.toggle("search-mobile__input--active");
+  searchMobileButton.classList.toggle("search-mobile__btn--change");
+  avatar.classList.toggle("hide");
+  searchCloseBtnMobile.classList.toggle("search-close-button--active");
 });
