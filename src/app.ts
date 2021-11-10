ef6842e62d0b515a66d29e086bc80ae0129ea898
Krystian Kalwik <ambitious156288@gmail.com>
2021-11-10 17:37:24 +0100

    nav done

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,16 @@ const menu = document.querySelector(".menu")!;
 const searchButtonBottom = document.querySelector(".search-laptop__btn")!;
 const searchInputBottom = document.querySelector(".search-laptop__input")!;
 
+const closeNavigationButton = document.querySelector(".menu-mobile__button")!;
+
 avatar.addEventListener("click", () => {
   menu.classList.toggle("menu--active");
 });
 
 searchButtonBottom.addEventListener("click", () => {
   searchInputBottom.classList.toggle("search-laptop--active");
+});
+
+closeNavigationButton.addEventListener("click", () => {
+  menu.classList.toggle("menu--active");
 });
