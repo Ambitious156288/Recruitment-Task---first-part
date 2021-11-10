ccbfd3a47237df139f0c6cbb6262a0c2d508fff6
Krystian Kalwik <ambitious156288@gmail.com>
2021-11-10 15:57:00 +0100

    new  feature - dropdown search - RWD laptop

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 const avatar = document.querySelector(".avatar")!;
 const menu = document.querySelector(".menu")!;
 
+const searchButtonBottom = document.querySelector(".search-laptop__btn")!;
+const searchInputBottom = document.querySelector(".search-laptop__input")!;
+
 avatar.addEventListener("click", () => {
   menu.classList.toggle("menu--active");
+});
+
+searchButtonBottom.addEventListener("click", () => {
+  searchInputBottom.classList.toggle("search-laptop--active");
 });
